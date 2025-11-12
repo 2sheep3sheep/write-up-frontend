c9e5ef33c61bab621abff3d7015df4a10ad4d3d1
Jan <opoceja1@fit.cvut.cz>
2025-11-12 13:43:46 +0100

    BackArrow component, material UI icons dependency

---
+++ src/components/generic/BackArrow.jsx
@@ -0,0 +1,13 @@
+
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+function BackArrow(props) {
+    return (
+        <button className='back-arrow'>
+            <ArrowBackIcon/>
+            Back
+        </button>
+    )
+}
+
+export default BackArrow;
