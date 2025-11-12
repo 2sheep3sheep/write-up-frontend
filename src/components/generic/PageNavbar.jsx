48d670318a85696abe1e85d39098984938fb1ed0
Jan <opoceja1@fit.cvut.cz>
2025-11-12 14:51:48 +0100

    NavBar component, styles

--- src/components/generic/PageNavbar.jsx
+++ src/components/generic/PageNavbar.jsx
@@ -4,10 +4,19 @@ import PersonIcon from '@mui/icons-material/Person';
 
 function PageNavbar(props) {
     return (
-        <div>
-            <HomeIcon style={{fontSize:"48px"}}/>
-            <MenuBookIcon style={{fontSize:"48px"}}/>
-            <PersonIcon style={{fontSize:"48px"}}/>
+        <div style={{width:"min-content", justifyContent:"space-evenly", display:"flex", height:"8vh"}}>
+            <button className='navbar-button navbar-left' style={{display:"block", width:"12vh", textAlign:"center", fontSize:"14px"}}>
+                <HomeIcon style={{fontSize:"30px"}}/>
+                 <div>Home</div>
+            </button>
+            <button className='navbar-button navbar-middle' style={{display:"block", width:"12vh", textAlign:"center", fontSize:"14px"}}>
+            <MenuBookIcon style={{fontSize:"30px"}}/>
+                 <div>My Books</div>
+            </button>
+            <button className='navbar-button navbar-right' style={{display:"block", width:"12vh", textAlign:"center", fontSize:"14px"}}>
+            <PersonIcon style={{fontSize:"30px"}}/>
+                 <div>Profile</div>
+            </button>
         </div>
     )
 }
