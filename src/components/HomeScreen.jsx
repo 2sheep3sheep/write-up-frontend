7835225d2a24965e78fcfd28885fb0f84be34d53
Jan <opoceja1@fit.cvut.cz>
2025-11-17 18:56:20 +0100

    Improved Home screen button breakpoints

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -2,6 +2,7 @@ import { ClipLoader } from "react-spinners";
 import { useState, useEffect } from 'react';
 import BackArrow from "./generic/BackArrow";
 import PageNavbar from "./generic/PageNavbar";
+import LogoutIcon from '@mui/icons-material/Logout';
 
 function HomeScreen({ setScreen }) {
     const [bookListCall, setBookListCall] = useState({ state: "inactive" });
@@ -35,12 +36,12 @@ function HomeScreen({ setScreen }) {
 
             <div style={{ width: "100%", minHeight: "10vh", fontSize: "20px", display: "flex", justifyContent: "space-between", boxSizing: "border-box", padding: "0 20px 0 20px" }} className="page-header">
                 <BackArrow onClick={e => setScreen("/")} />
-                <button className="basic-btn" onClick={handleLogout} style={{ width: "10vh", backgroundColor: "var(--bg-bottom)" }}>{
-                    logoutCall.state === "pending" ? <ClipLoader color="var(--color-primary)" size={20} /> : "Logout"
+                <button className="basic-btn" onClick={handleLogout} style={{ width: "9%", backgroundColor: "var(--bg-bottom)"}}>{
+                    logoutCall.state === "pending" ? <ClipLoader color="var(--color-primary)" size={20} /> : (<div style={{display:"flex", alignItems:"center", textAlign:"center"}}><LogoutIcon/> Logout</div>)
                 }
                 </button>
 
-                <h1 style={{ textAlign: 'center', position: "absolute", left: "10%", right: "10%", marginTop: 50 }}>Home (placeholder)</h1>
+                <h1 style={{ textAlign: 'center', position: "absolute", left: "10%", right: "10%", marginTop: 50 }}  className="home-screen-title">Home (placeholder)</h1>
             </div>
 
             <div style={{ minHeight: '100vh', padding: 20 }}>
