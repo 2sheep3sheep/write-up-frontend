f4485a910a2e3e42f2a86fedf9c0295015d7b8f3
Jiří Kerner <jirikernerjr@gmail.com>
2025-12-04 13:30:56 +0100

    Project structure adjustments

---
+++ src/components/ProfileEditForm.jsx
@@ -0,0 +1,10 @@
+// Komponenta s formulářem pro upravení profilu autora
+
+function ProfileEditForm({ onProfileEdited = () => { }, onEditCancel = () => { } }) {
+    return (
+        <>
+        </>
+    )
+}
+
+export default ProfileEditForm;
